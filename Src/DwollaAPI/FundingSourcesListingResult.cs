a3f1cff73961ec7eb93e258ced59295a8a919721
Robert Greathouse <robert@dwolla.com>
2012-08-29 16:46:05 -0500

    Adding funding sources endpoints

---
+++ Src/DwollaAPI/FundingSourcesListingResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Dwolla.API.DwollaAPI
+{
+    public class FundingSourcesListingResult
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [JsonProperty("Type")]
+        public string Type { get; set; }
+
+        [JsonProperty("Verified")]
+        public bool Verified { get; set; }
+    }
+}
